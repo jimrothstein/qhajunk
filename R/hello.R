852593a227931f7a2dd77b16d84cd4b40ea038b8
jimrothstein <jimrothstein@gmail.com>
2024-11-21 19:31:09 -0800

    First commit

---
+++ R/hello.R
@@ -0,0 +1,26 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   https://r-pkgs.org
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+# jr adds
+library(devtools)
+library(usethis)
+
+# READ:  https://usethis.r-lib.org/reference/use_github_action.html
+usethis::use_github_action("check-standard.yaml")
+
+1
+hello <- function() {
+  print("Hello, world!")
+}
